348a976eb7343ec746e06aa4147f853eb1acb860
viardpa <Paul.Viard@etu.univ-grenoble-alpes.fr>
2022-05-09 22:50:50 +0200

    V1 : Comparaison de 2 images MAIS de même taille.

--- src/ComparaisonImage.java
+++ src/ComparaisonImage.java
@@ -37,10 +37,32 @@ public class ComparaisonImage {
                 int redB = (rgbB >> 16) & 0xff;
                 int greenB = (rgbB >> 8) & 0xff;
                 int blueB = (rgbB) & 0xff;
+                difference += Math.abs(redA - redB);
+                difference += Math.abs(greenA - greenB);
+                difference += Math.abs(blueA - blueB);
 
             }
         }
+        // Total number of red pixels = width * height
+        // Total number of blue pixels = width * height
+        // Total number of green pixels = width * height
+        // So total number of pixels = width * height * 3
+        double total_pixels = width1 * height1 * 3;
 
+        // Normalizing the value of different pixels
+        // for accuracy(average pixels per color
+        // component)
+        double avg_different_pixels = difference /
+                total_pixels;
 
+        // There are 255 values of pixels in total
+        double percentage = (avg_different_pixels /
+                255) * 100;
+
+        System.out.println("Difference Percentage-->" +
+                percentage);
     }
 }
+
+
+
